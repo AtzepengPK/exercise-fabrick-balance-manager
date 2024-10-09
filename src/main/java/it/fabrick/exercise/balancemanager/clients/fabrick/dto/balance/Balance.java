a6fba9df4ee770ec55af004a99560dfde417a303
Sergiu Andrei Axinte <sergiu.andrei.axinte@external.toyotaconnected.eu>
2024-10-09 18:52:08 +0200

    adeed balance record

---
+++ src/main/java/it/fabrick/exercise/balancemanager/clients/fabrick/dto/balance/Balance.java
@@ -0,0 +1,9 @@
+package it.fabrick.exercise.balancemanager.clients.fabrick.dto.balance;
+
+public record Balance(
+	Double availableBalance,
+	Double balance,
+	String currency,
+	String date
+) {
+};
